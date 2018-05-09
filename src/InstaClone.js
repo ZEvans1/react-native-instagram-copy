7597f51744b9ac2b0a9cdf6708a4b826d84364a4
zach <zte.zachary@gmail.com>
2018-05-08 21:33:11 -0700

    Remove initial filler material, create directories, add test text.

---
+++ src/InstaClone.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+class InstaClone extends Component {
+
+    render(){
+        return(
+            <View>
+                <Text>Hello</Text>
+            </View>
+        )
+    }
+}
+
+export default InstaClone;
