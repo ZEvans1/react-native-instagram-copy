0dd1789559359ae6526be981317ba6e0ea773e38
zach <zte.zachary@gmail.com>
2018-05-09 13:03:39 -0700

    Start top navbar.

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -6,9 +6,11 @@ class InstaClone extends Component {
     render(){
         return(
             <View style={{ flex:1, width: 100 + "%", height:100 + "%"}}>
-                <Text>Hello</Text>
+                <View style={styles.tempNav}>
+                    <Text>Not Instagram</Text>
+                </View>
                 <Image
-                    style={{width: 100 + "%", height: 100 + "%"}}
+                    style={{width: 100 + "%", height: 100}}
                     source={{
                         uri:
                             "https://www.thebrandbite.com/wp-content/media/2015/07/apple-7.jpg"
@@ -18,5 +20,15 @@ class InstaClone extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    tempNav: {
+        width: 100+"%", 
+        height: 70, 
+        backgroundColor: "rgb(250,250,250)",
+        borderBottomColor: "rgb(233,233,233)",
+        borderBottomWidth: StyleSheet.hairlineWidth
+    }
+})
 
 export default InstaClone;
