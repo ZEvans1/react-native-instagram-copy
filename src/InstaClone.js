0443cbb9840ecba984323628e27ddfcbf68319d7
zach <zte.zachary@gmail.com>
2018-05-10 09:19:12 -0700

    Update top bars.

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -7,7 +7,21 @@ class InstaClone extends Component {
         return(
             <View style={{ flex:1, width: 100 + "%", height:100 + "%"}}>
                 <View style={styles.tempNav}>
-                    <Text>Not Instagram</Text>
+                    <Text>Not_Instagram</Text>
+                </View>
+                <View style={styles.userBar}/>
+                <View style={{ flexDirection: "row" }}>
+                    <Image
+                        style={{ width:40, height:40}} 
+                        source ={{
+                        uri:
+                            "https://cdn1.iconfinder.com/data/icons/instagram-ui-glyph/48/Sed-09-128.png"
+                        }}
+                    />
+                    <Text>User_Name</Text>
+                </View>
+                <View>
+
                 </View>
                 <Image
                     style={{width: 100 + "%", height: 100}}
@@ -24,10 +38,20 @@ class InstaClone extends Component {
 const styles = StyleSheet.create({
     tempNav: {
         width: 100+"%", 
-        height: 70, 
+        height: 56,
+        marginTop: 20, 
         backgroundColor: "rgb(250,250,250)",
         borderBottomColor: "rgb(233,233,233)",
-        borderBottomWidth: StyleSheet.hairlineWidth
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    userBar: {
+        width: 100 + "%",
+        height: 50,
+        backgroundColor: "rgb(255,255,255)",
+        flexDirection: "row"
     }
 })
 
