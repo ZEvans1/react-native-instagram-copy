46583006e8c12beca5dc4142bbff99bb6c224ae1
zach <zte.zachary@gmail.com>
2018-05-10 14:10:53 -0700

    Add styling and ellipse place holder in top.

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -9,19 +9,20 @@ class InstaClone extends Component {
                 <View style={styles.tempNav}>
                     <Text>Not_Instagram</Text>
                 </View>
-                <View style={styles.userBar}/>
-                <View style={{ flexDirection: "row" }}>
-                    <Image
-                        style={{ width:40, height:40}} 
-                        source ={{
-                        uri:
-                            "https://cdn1.iconfinder.com/data/icons/instagram-ui-glyph/48/Sed-09-128.png"
-                        }}
-                    />
-                    <Text>User_Name</Text>
-                </View>
-                <View>
-
+                <View style={styles.userBar}>
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Image
+                            style={styles.userPic} 
+                            source ={{
+                            uri:
+                                "https://cdn1.iconfinder.com/data/icons/instagram-ui-glyph/48/Sed-09-128.png"
+                            }}
+                        />
+                        <Text style={{ marginLeft: 10 }}>User_Name</Text>
+                        </View>
+                    <View>
+                        <Text style={{ fontSize: 36 }}>...</Text>
+                    </View>
                 </View>
                 <Image
                     style={{width: 100 + "%", height: 100}}
@@ -31,7 +32,7 @@ class InstaClone extends Component {
                     }}
                 />
             </View>
-        )
+        );
     }
 }
 
@@ -51,7 +52,15 @@ const styles = StyleSheet.create({
         width: 100 + "%",
         height: 50,
         backgroundColor: "rgb(255,255,255)",
-        flexDirection: "row"
+        flexDirection: "row",
+        paddingHorizontal: 10,
+        justifyContent: "space-between"
+    },
+
+    userPic: {
+        height: 40,
+        width: 40,
+        borderRadius: 20
     }
 })
 
