3cc43a8fff0b3381a1837d9e5e4c470db4923377
zach <zte.zachary@gmail.com>
2018-05-13 17:14:46 -0700

    Move code into screens/MainFeed.

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native';
-import config from "./config";
-import { PostFeed } from './components/container'
+import { View, StyleSheet } from 'react-native';
+import { MainFeed } from './components/screens';
 
 class InstaClone extends Component {
-
-    render(){
-    
+    render() {
         return(
-            <View style={{ flex:1, width: 100 + "%", height:100 + "%"}}>
-                <View style={styles.tempNav}>
-                    <Text>Not_Instagram</Text>
-                </View>
-                <PostFeed/>
-            </View>
-        );
+            <MainFeed />
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    tempNav: {
-        width: 100+"%", 
-        height: 56,
-        marginTop: 20, 
-        backgroundColor: "rgb(250,250,250)",
-        borderBottomColor: "rgb(233,233,233)",
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-})
+
 
 export default InstaClone;
